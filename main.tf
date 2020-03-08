9f9a0c8bc3cc56d7bf6eaeab74550e0eefc6da52
xichen@blueplanet.com <xichen@blueplanet.com>
2020-03-08 21:17:44 +1100

    change var.ami

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 
 # Create EC2 instance
 resource "aws_instance" "default" {
-  ami                    = "${var.ami}"
+  ami                    = var.ami
   count                  = var.instance_count
   key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.default.id]
