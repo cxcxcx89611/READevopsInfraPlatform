0a184eed4de8bd4a0180c9906ff274f28e89b51b
xichen@blueplanet.com <xichen@blueplanet.com>
2020-03-08 12:21:51 +1100

    first commit for jenkins and terraform integration

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "instance_count" {
+  default = 1
+}
+
+variable "key_name" {
+  description = "Private key name to use with instance"
+  default     = "terraform"
+}
+
+variable "instance_type" {
+  description = "AWS instance type"
+  default     = "t2.micro"
+}
+
+variable "ami" {
+  description = "Base AMI to launch the instances"
+
+  # Bitnami NGINX AMI
+  default = "ami-0f47020f179ec7aa5"
+}
