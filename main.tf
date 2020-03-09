948a9677b50a5b6165547119b9e30d0a895c4986
xichen@blueplanet.com <xichen@blueplanet.com>
2020-03-09 22:40:27 +1100

    change inboud rule

--- main.tf
+++ main.tf
@@ -52,7 +52,7 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-      from_port   = 0
+      from_port   = 9292
       to_port     = 9292
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
