cbb9d1ab76d299892be5f735e6d28e96b5afc47c
xichen@blueplanet.com <xichen@blueplanet.com>
2020-03-09 22:35:34 +1100

    change the protocal and add 9292 port number ingress

--- main.tf
+++ main.tf
@@ -38,11 +38,25 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
+      from_port   = 8080
+      to_port     = 80
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+      from_port   = 0
+      to_port     = 9292
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 
   egress {
     from_port   = 0
