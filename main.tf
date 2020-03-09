64b4bd0d101fba05a69c4022b19445ae87e20bc9
xichen@blueplanet.com <xichen@blueplanet.com>
2020-03-09 22:37:41 +1100

    change outbound rule

--- main.tf
+++ main.tf
@@ -38,8 +38,8 @@ resource "aws_security_group" "default" {
   }
 
   ingress {
-      from_port   = 8080
-      to_port     = 80
+      from_port   = 80
+      to_port     = 8080
       protocol    = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
     }
